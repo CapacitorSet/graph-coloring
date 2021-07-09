e95c83f5544ad39c67bb1d74177966ee0d828968
CapacitorSet <capacitorset@gmail.com>
2021-07-09 11:32:22 +0200

    Refactor LubySolver

--- solve/LubySolver.cpp
+++ solve/LubySolver.cpp
@@ -3,7 +3,7 @@
 #include <random>
 #include <set>
 
-LubySolver::LubySolver() {
+LubySolver::LubySolver() : gen(RANDOM_SEED) {
     this->name = "LubySolver";
 }
 
@@ -11,7 +11,7 @@ void LubySolver::solve(Graph &original_graph) {
     Graph graph(original_graph);
     color_t color = 0;
     while (!graph.empty()) {
-        std::vector<uint32_t> MIS = maximal_independent_set(graph);
+        compute_MIS(graph);
         for (uint32_t vertex : MIS) {
             original_graph.colors[vertex] = color;
             graph.remove_vertex(vertex);
@@ -22,84 +22,92 @@ void LubySolver::solve(Graph &original_graph) {
 
 // Best explained here:
 // https://en.wikipedia.org/wiki/Maximal_independent_set#Random-selection_parallel_algorithm_[Luby's_Algorithm]
-std::vector<uint32_t> LubySolver::maximal_independent_set(const Graph &src) {
-    std::mt19937 gen(RANDOM_SEED);
+void LubySolver::compute_MIS(const Graph &src) {
+    // Reset solver state
+    MIS.clear();
+    V.clear();
 
-    std::vector<uint32_t> I;
-    std::set<uint32_t> V;
     for (uint32_t i = 0; i < src.vertices.size(); i++)
         // It suffices to check for is_deleted here, since we don't delete vertices inside the function
         if (!src.is_deleted(i))
             V.emplace(i);
 
-    // Check if empty _wrt src.deleted_!
     while (!V.empty()) {
         // The subset of vertices selected
         // We use std::vector as the iterator for std::set is very slow
-        std::vector<uint32_t> S;
-        // For each vertex, include it or not with probability 1/(2/d(v))
-        for (uint32_t i : V) {
-            double probability = 1. / (2 * src.degree_of(i));
-            std::bernoulli_distribution d(probability);
-            if (d(gen))
-                S.emplace_back(i);
-        }
-        // Optimization: do not check for edges if we have less than 2 nodes
-        if (S.size() >= 2) {
-            // For each edge in E, check that "from" and "to" are in the graph.
-            // Because src.vertices is sorted, and the vector of edges is also sorted in the Metis parser, we can use a
-            // more efficient algo: set_intersection.
+        std::vector<uint32_t> S = probabilistic_select(src);
 
-            // First, check that the "from" index is in the graph S.
-            std::vector<uint32_t> from_candidates, from_indices(src.vertices.size());
-            from_candidates.reserve(std::max(S.size(), src.vertices.size()));
-            for (uint32_t i = 0; i < src.vertices.size(); i++)
-                from_indices[i] = i;
-
-            std::set_intersection(
-                    from_indices.cbegin(), from_indices.cend(),
-                    S.cbegin(), S.cend(),
-                    std::back_inserter(from_candidates));
-
-            for (uint32_t from : from_candidates) {
-                // Note that we do not need to check for is_deleted.
-                // If an element is in S it was in V, which we already checked.
-
-                // Then, check that the "to" index is in the graph S.
-                const edges_t &neighbors = src.vertices[from];
-                std::vector<uint32_t> edges_in_graph;
-                edges_in_graph.reserve(std::max(S.size(), neighbors.size()));
-                // If we already counted 1 -> 2, do not count 2 -> 1 again:
-                // we start searching at the first neighbor higher than the "from" vertex
-                auto first_new_neighbor = std::lower_bound(neighbors.cbegin(), neighbors.cend(), from);
-                std::set_intersection(
-                        first_new_neighbor, neighbors.cend(),
-                        S.cbegin(), S.cend(),
-                        std::back_inserter(edges_in_graph));
-
-                for (uint32_t to : edges_in_graph) {
-                    if (src.degree_of(from) <= src.degree_of(to)) {
-                        // std::lower_bound is std::find for sorted containers.
-                        auto from_pos = std::lower_bound(S.cbegin(), S.cend(), from);
-                        // The item may not be found if we already removed it
-                        if (from_pos != S.cend())
-                            S.erase(from_pos);
-                    } else {
-                        auto to_pos = std::lower_bound(S.cbegin(), S.cend(), to);
-                        if (to_pos != S.cend())
-                            S.erase(to_pos);
-                    }
-                }
-            }
-        }
+        remove_edges(S, src);
 
         for (uint32_t v : S) {
-            I.emplace_back(v);
+            MIS.emplace_back(v);
             V.erase(v);
             for (uint32_t neighbor : src.neighbors_of(v))
                 V.erase(neighbor);
         }
     }
+}
 
-    return I;
+std::vector<uint32_t> LubySolver::probabilistic_select(const Graph &graph) {
+    std::vector<uint32_t> S;
+    // For each vertex, include it or not with probability 1/(2/d(v))
+    for (uint32_t i : V) {
+        double probability = 1. / (2 * graph.degree_of(i));
+        std::bernoulli_distribution d(probability);
+        if (d(gen))
+            S.emplace_back(i);
+    }
+    return S;
+}
+
+void LubySolver::remove_edges(std::vector<uint32_t> &S, const Graph &g) {
+    // Optimization: do not check for edges if we have less than 2 nodes
+    if (S.size() < 2)
+        return;
+
+    // For each edge in E, check that "from" and "to" are in the graph.
+    // Because g.vertices is sorted, and the vector of edges is also sorted in
+    // the Metis parser, we can use a more efficient algo: set_intersection.
+
+    // First, check that the "from" index is in the graph S.
+    std::vector<uint32_t> from_candidates, from_indices(g.vertices.size());
+    from_candidates.reserve(std::max(S.size(), g.vertices.size()));
+    for (uint32_t i = 0; i < g.vertices.size(); i++)
+        from_indices[i] = i;
+
+    std::set_intersection(
+            from_indices.cbegin(), from_indices.cend(),
+            S.cbegin(), S.cend(),
+            std::back_inserter(from_candidates));
+
+    for (uint32_t from : from_candidates) {
+        // Note that we do not need to check for is_deleted.
+        // If an element is in S it was in V, which we already checked.
+
+        // Then, check that the "to" index is in the graph S.
+        const edges_t &neighbors = g.vertices[from];
+        std::vector<uint32_t> edges_in_graph;
+        edges_in_graph.reserve(std::max(S.size(), neighbors.size()));
+        // If we already counted 1 -> 2, do not count 2 -> 1 again:
+        // we start searching at the first neighbor higher than the "from" vertex
+        auto first_new_neighbor = std::lower_bound(neighbors.cbegin(), neighbors.cend(), from);
+        std::set_intersection(
+                first_new_neighbor, neighbors.cend(),
+                S.cbegin(), S.cend(),
+                std::back_inserter(edges_in_graph));
+
+        for (uint32_t to : edges_in_graph) {
+            if (g.degree_of(from) <= g.degree_of(to)) {
+                // std::lower_bound is std::find for sorted containers.
+                auto from_pos = std::lower_bound(S.cbegin(), S.cend(), from);
+                // The item may not be found if we already removed it
+                if (from_pos != S.cend())
+                    S.erase(from_pos);
+            } else {
+                auto to_pos = std::lower_bound(S.cbegin(), S.cend(), to);
+                if (to_pos != S.cend())
+                    S.erase(to_pos);
+            }
+        }
+    }
 }
--- solve/LubySolver.h
+++ solve/LubySolver.h
@@ -1,10 +1,21 @@
 #ifndef GRAPH_COLORING_LUBYSOLVER_H
 #define GRAPH_COLORING_LUBYSOLVER_H
 
+#include <random>
+#include <set>
 #include "Solver.h"
 
 class LubySolver : public Solver {
-    std::vector<uint32_t> maximal_independent_set(const Graph &src);
+    std::mt19937 gen;
+    std::vector<uint32_t> MIS;
+    std::set<uint32_t> V;
+
+    // First step
+    inline std::vector<uint32_t> probabilistic_select(const Graph &);
+    // Second step
+    inline void remove_edges(std::vector<uint32_t> &S, const Graph &);
+    // Luby's algorithm
+    void compute_MIS(const Graph &src);
 
 public:
     LubySolver();
