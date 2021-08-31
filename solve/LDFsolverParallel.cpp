cf49b33b966bcda66682b20655ea21dc5a043980
Amr Alashram s289417 <s289417@studenti.polito.it>
2021-08-31 16:50:06 +0200

    LDF Parallel and Random Priority are updated

--- solve/LDFsolverParallel.cpp
+++ solve/LDFsolverParallel.cpp
@@ -1,19 +1,30 @@
 #include "LDFsolverParallel.h"
 
+
 LDFsolverParallel::LDFsolverParallel(int num_threads) : num_threads(num_threads) {}
 
+
 void LDFsolverParallel::solve(Graph &original_graph) {
+
     uint32_t num_vertices = original_graph.vertices.size();
+
     std::vector<uint32_t> degrees(num_vertices, 0);
+
     std::vector<std::thread> thread_Pool;
 
+
     for (uint32_t vertex = 0; vertex < num_vertices; vertex++) {
-        thread_Pool.emplace_back([&vertex, &original_graph, &degrees, this]() {
+
+        thread_Pool.emplace_back(std::thread([&vertex, &original_graph, &degrees, this]() {
+
             // Each vertex computes its degree and announce it.
             m.lock();
+
             degrees[vertex] = original_graph.degree_of(vertex);
+
             m.unlock();
-        });
+
+        }));
     }
 
     for (auto &th : thread_Pool) {
@@ -21,20 +32,31 @@ void LDFsolverParallel::solve(Graph &original_graph) {
     }
 
     std::vector<uint32_t> vertices_to_color(degrees.size());
+
     compute_vertices_to_color_in_order(degrees, vertices_to_color);
 
-    for (uint32_t vertex_to_color : vertices_to_color) {
+    for (uint32_t vertex_to_color : vertices_to_color) {s
         original_graph.color_with_smallest(vertex_to_color);
     }
 }
 
+
 void LDFsolverParallel::compute_vertices_to_color_in_order(std::vector<uint32_t> &degrees, std::vector<uint32_t> &vertices_to_color) {
+
     std::iota(vertices_to_color.begin(), vertices_to_color.end(), 0);
+
     std::stable_sort(vertices_to_color.begin(), vertices_to_color.end(), [&](int i, int j) {
+
         return degrees[i] > degrees[j];
+
     });
+
 }
 
+
 std::string LDFsolverParallel::name() const {
+
     return "LDFsolverParallel (" + std::to_string(num_threads) + " threads)";
+
 }
+
--- solve/RandomPrioritySolver.cpp
+++ solve/RandomPrioritySolver.cpp
@@ -1,35 +1,54 @@
 #include "RandomPrioritySolver.h"
 
+
 RandomPrioritySolver::RandomPrioritySolver(int num_threads) : num_threads(num_threads) {}
 
 void RandomPrioritySolver::solve(Graph &original_graph) {
+
     Graph uncolored_graph(original_graph);
+
     color_t color = 0;
+
     while (!uncolored_graph.empty()) {
+
         compute_MIS(uncolored_graph);
+
         for (uint32_t vertex : MIS) {
+
             original_graph.colors[vertex] = color;
+
             uncolored_graph.remove_vertex(vertex);
+
         }
         color++;
+
     }
+
 }
 
 // Best explained here:
 // https://en.wikipedia.org/wiki/Maximal_independent_set#Random-priority_parallel_algorithm
 
 void RandomPrioritySolver::compute_MIS(const Graph &src) {
+
     // Reset solver state
+
     MIS.clear();
+
     Remaining_Vertices.clear();
 
     uint32_t num_verticies_graph = src.vertices.size();
 
     for (uint32_t i = 0; i < num_verticies_graph; i++) {
+
         // It suffices to check for is_deleted here, since we don't delete vertices inside the function
+
         if (!src.is_deleted(i)) {
+
             Remaining_Vertices.emplace_back(i);
+
         }
+
     }
 
     uint32_t num_vertices_Remaining = Remaining_Vertices.size();
@@ -39,19 +58,25 @@ void RandomPrioritySolver::compute_MIS(const Graph &src) {
     Destroy_Vec.assign(num_verticies_graph, false);
 
     while (!Remaining_Vertices.empty()) {
+
         std::vector<std::thread> threadPool;
 
         pthread_barrier_init(&barrier1, NULL, num_vertices_Remaining);
+
         pthread_barrier_init(&barrier2, NULL, num_vertices_Remaining);
 
         for (uint32_t thID = 0; thID < num_vertices_Remaining; thID++) {
-            threadPool.emplace_back([&thID, &src, this] () {
+
+            threadPool.emplace_back(std::thread([&thID, &src, this] () {
 
                 uint32_t vertexID = Remaining_Vertices[thID];
+
                 // Select random number
+
                 uint32_t RandNum = rand() % 100;
 
                 // Send it to the neighbours "Announce it"
+
                 Random_Priority_Vec[vertexID] = RandNum;
 
                 edges_t neighbors = src.neighbors_of(vertexID);
@@ -67,29 +92,45 @@ void RandomPrioritySolver::compute_MIS(const Graph &src) {
                     if (std::find(Remaining_Vertices.begin(), Remaining_Vertices.end(), neighbor) != Remaining_Vertices.end()) {
 
                         remaining_neighbors.emplace_back(neighbor);
+
                         if (Random_Priority_Vec[neighbor] < RandNum) {
+
                             IamTheSmallest = false;
+
                             break;
+
                         }
+
                     }
 
                 }
 
                 if (IamTheSmallest) {
+
                     // Insert itself into MIS
+
                     wrt_mutex.lock();
+
                     MIS.emplace_back(vertexID);
+
                     wrt_mutex.unlock();
 
                     // Remove itself from Remaining_Vertices
+
                     std::remove(Remaining_Vertices.begin(), Remaining_Vertices.end(), vertexID);
 
                     // Tells the neighbors
+
                     for (uint32_t neighbor : remaining_neighbors) {
+
                         wrt_mutex.lock();
+
                         Destroy_Vec[neighbor] = true;
+
                         wrt_mutex.unlock();
+
                     }
+
                 }
 
                 pthread_barrier_wait(&barrier2);
@@ -98,17 +139,22 @@ void RandomPrioritySolver::compute_MIS(const Graph &src) {
                     // Remove itself from Remaining_Vertices
                     std::remove(Remaining_Vertices.begin(), Remaining_Vertices.end(), vertexID);
                 }
-            });
+            }));
         }
+
         for (auto &th : threadPool) {
+
             th.join();
+
         }
     }
 }
 
-
-
 std::string RandomPrioritySolver::name() const {
+
     return "RandomPrioritySolver (" + std::to_string(num_threads) + " threads)";
+
 }
+
+
 
