4c2223e4308096bc2fae72ddccb48bae2b3a34bd
CapacitorSet <capacitorset@gmail.com>
2021-09-15 22:12:07 +0200

    Optimize LDFparallelSolver

--- solve/LDFparallelSolver.cpp
+++ solve/LDFparallelSolver.cpp
@@ -1,5 +1,6 @@
 #include "LDFparallelSolver.h"
-#include <stdio.h>
+#include <numeric>
+#include <thread>
 
 LDFparallelSolver::LDFparallelSolver(int num_threads) : num_threads(num_threads) {}
 
@@ -8,66 +9,47 @@ void LDFparallelSolver::solve(Graph &graph) {
     uint32_t num_vertices = graph.vertices.size();
 
     std::vector<uint32_t> degrees(num_vertices, 0);
-    std::vector<uint32_t> weights(num_vertices, 0);
 
     /* computing the number of vertices to be done per one thread */
     uint32_t vertices_per_thread = num_vertices / num_threads;
     uint32_t remaining_vertices = num_vertices % num_threads;
 
     /* if a number of threads larger than the ability of the system, generate error */
-    if (num_threads > std::thread::hardware_concurrency()) {
-        std::cout << "The largest number of threads can be used is: " << std::thread::hardware_concurrency() << std::endl;
-        perror("Very large number of threads!! Please, use a smaller number !\n");
-        return;
-    }
+    if (num_threads > std::thread::hardware_concurrency())
+        throw std::runtime_error("Hardware concurrency exceeded: please use at most " +
+                                 std::to_string(std::thread::hardware_concurrency()) + " threads");
 
-    /* if a number of threads greater than the number of vertices, generate error */
-    else if (num_threads > num_vertices) {
-        perror("The entered number of threads is larger than number of vertices!! Please, use a number that is less than or equal the number of vertices\n");
-        return;
-    }
+    // todo: fix
+    if (num_threads > num_vertices)
+        throw std::runtime_error("More threads than vertices: please use at most " +
+                                 std::to_string(num_vertices) + " threads");
 
-    else {
+    /* Each thread has a vertex to start from and a range of vertices to work on */
+    uint32_t vertex = 0;
+    uint32_t range = vertices_per_thread;
+    uint32_t thread_counter = num_threads;
 
-        /* Each thread has a vertex to start from and a range of vertices to work on */
-        uint32_t vertex = 0;
-        uint32_t range = vertices_per_thread;
-        uint32_t thread_counter = num_threads;
+    std::vector<std::thread> threads;
 
-        std::vector<std::thread> thread_Pool;
-
-        while (thread_counter) {
-            if (thread_counter == 1) {
-                range = vertices_per_thread + remaining_vertices;
-            }
-
-            thread_Pool.emplace_back(std::thread([vertex, &range, &graph, &weights, &degrees, this]() {
-                
-                /* Applying the weighting phase where each vertex takes a weight according to a particular algorithm */
-                compute_degrees(graph, degrees, vertex, (range + vertex));
-
-                /* Applying the coloring phase where coloring is done in order according to the assigned weights */
-                coloring_in_parallel(degrees, vertex, range, graph);
-            }));
-            vertex += vertices_per_thread;
-            thread_counter--;
+    while (thread_counter) {
+        if (thread_counter == 1) {
+            range = vertices_per_thread + remaining_vertices;
         }
 
-        for (auto &th : thread_Pool) {
-            th.join();
-        }
+        threads.emplace_back(std::thread([vertex, &range, &graph, &degrees, this]() {
+            /* Applying the coloring phase where coloring is done in order according to the assigned weights */
+            coloring_in_parallel(degrees, vertex, range, graph);
+        }));
+        vertex += vertices_per_thread;
+        thread_counter--;
+    }
+
+    for (auto &th : threads) {
+        th.join();
     }
 }
 
-void LDFparallelSolver::compute_degrees(Graph &original_graph, std::vector<uint32_t> &degrees, const uint32_t vertex, const uint32_t &upper_bound) {
-
-    /*Each vertex computes its degree and announcing it by saving it in a shared vector */
-    for (uint32_t vertexID = vertex; vertexID < upper_bound; vertexID++) {
-        degrees[vertexID] = original_graph.degree_of(vertexID);
-    }
-}
-
-void LDFparallelSolver::coloring_in_parallel(std::vector<uint32_t> &degrees, uint32_t vertex, const uint32_t range, Graph &original_graph) {
+void LDFparallelSolver::coloring_in_parallel(std::vector<uint32_t> &degrees, uint32_t vertex, uint32_t range, Graph &original_graph) {
 
     /* Create a vector to represent the vertices to be colored in order and intialize it in ascending order */
     std::vector<uint32_t> vertices_to_color(range);
@@ -75,7 +57,7 @@ void LDFparallelSolver::coloring_in_parallel(std::vector<uint32_t> &degrees, uin
 
     /* Reorder the vactor according to be descendingly from the highest weight to the lowest weight */
     std::stable_sort(vertices_to_color.begin(), vertices_to_color.end(), [&](int i, int j) {
-        return degrees[i + vertex] > degrees[j + vertex];
+        return original_graph.degree_of(i + vertex) > original_graph.degree_of(j + vertex);
     });
 
     /* start coloring according to the order assigned above where no two neighbors have the same color */
--- solve/LDFparallelSolver.h
+++ solve/LDFparallelSolver.h
@@ -1,23 +1,12 @@
 #ifndef GRAPH_COLORING_LDFPARALLELSOLVER_H
-#define GRAPH_COLORING_LDFPARELLELSOLVER_H
+#define GRAPH_COLORING_LDFPARALLELSOLVER_H
 
 #include "Solver.h"
-#include <random>
-#include <set>
-#include <iostream>
-#include <algorithm>
-#include <random>
-#include <vector>
-#include <thread>
-#include <mutex>
-#include <numeric>
-#include <condition_variable>
 
 class LDFparallelSolver : public Solver {
     uint32_t num_threads;
     
-    void compute_degrees(Graph &original_graph, std::vector<uint32_t> &degrees, const uint32_t vertex, const uint32_t &upper_bound);
-    void coloring_in_parallel(std::vector<uint32_t> &degrees, uint32_t vertex, const uint32_t range, Graph &original_graph);
+    void coloring_in_parallel(std::vector<uint32_t> &degrees, uint32_t vertex, uint32_t range, Graph &original_graph);
 
 public:
     LDFparallelSolver(int num_threads);
